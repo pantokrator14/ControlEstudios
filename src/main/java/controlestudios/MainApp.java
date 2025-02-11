e6960e95825d8dfa80bb6ae5a2365c155070e16e
iulius <juliusjosepham@proton.me>
2025-02-11 13:35:28 -0400

    trying to solve problems with javaFX

--- src/main/java/controlestudios/MainApp.java
+++ src/main/java/controlestudios/MainApp.java
@@ -1,4 +1,16 @@
 package controlestudios;
 
-public class MainApp {
+public class MainApp extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(
+                getClass().getResource("/views/login.fxml") // Ruta desde resources/
+        );
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
